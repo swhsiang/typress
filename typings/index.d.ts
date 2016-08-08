27e19e4a33bd430e0bff9d1481d11d355b2c17db
swh <shih777577@gmail.com>
2016-08-09 01:06:24 +0800

    Add lodash package

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="globals/body-parser/index.d.ts" />
 /// <reference path="globals/express-serve-static-core/index.d.ts" />
 /// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/mime/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
